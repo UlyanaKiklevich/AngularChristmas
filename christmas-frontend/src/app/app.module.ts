79ecd0d86ac6d576c7c28de7e1b6a5ea0d898877
ymaroz <yuramoroz79@gmail.com>
2022-07-10 15:30:41 +0200

    added login component

--- christmas-frontend/src/app/app.module.ts
+++ christmas-frontend/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+          {path: 'login', component: LoginComponent}
+        ])
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
