4f9b5f2b3b097fead1bd8803b040fa2aef8ebd8d
ymaroz <yuramoroz79@gmail.com>
2022-07-13 22:17:46 +0200

    fixed routing

--- christmas-frontend/src/app/app.module.ts
+++ christmas-frontend/src/app/app.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {RouterModule} from "@angular/router";
+import {Routes, RouterModule} from "@angular/router";
 import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: AppComponent}
+];
 
 @NgModule({
   declarations: [
@@ -12,10 +17,11 @@ import { LoginComponent } from './login/login.component';
   ],
     imports: [
         BrowserModule,
-        RouterModule.forRoot([
-          {path: 'login', component: LoginComponent}
-        ])
+        RouterModule.forRoot(appRoutes)
     ],
+  exports: [
+    RouterModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
